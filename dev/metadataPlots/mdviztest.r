e20f5ce09c5cb62d31842d16bec7c0681bd053c3
Carolin Odebrecht <carolin.odebrecht@hu-berlin.de>
2019-02-20 19:08:54 +0100

    remove not useful lines

--- dev/metadataPlots/mdviztest.r
+++ dev/metadataPlots/mdviztest.r
@@ -32,49 +32,10 @@ mosaic(~ length.cat + year.cat + gender.cat, data=mdtest,
        labeling = labeling_values,)
 
 
-### pie chart
-sum<-table(mdfra$counter)
-sum
-sumyear<-table(mdfra$year.cat)
-sumyear
-
-sumgen<-table(mdfra$gender.cat)
-sumgen
-table(mdfra)
-round(table(mdfra) / length(gender.cat) *100 ,2)
-
-
-install.packages(MASS)
-fold<- table(sum$sumyear)
-fold
-pie(fold)
-
-### ??
-ggplot(mdfra, aes(x=year, y=length, color=gender.cat, shape=gender.cat))+
-  geom_vline(xintercept = c(1860,1880,1900), color="darkgray")+
-  geom_point(size=3, alpha=0.8)+
-  facet_wrap(~gender.cat~year.cat)+
-  theme_minimal()
-
 ### plot between fem/male per time slot, y length
-ggplot(mdfra, aes(x=year, y=length, color=gender.cat, shape=gender.cat))+
+ggplot(mdtest, aes(x=year, y=length, color=gender.cat, shape=gender.cat))+
   geom_vline(xintercept = c(1860,1880,1900), color="darkgray")+
   geom_point(size=3, alpha=0.8)+
   facet_wrap(~gender.cat~year.cat)+
   theme_minimal()
 
-### plot per topic
-ggplot(mdfra, aes(x=year, y=length, color=gender.cat, shape=gender.cat))+
-  geom_vline(xintercept = c(1860,1880,1900), color="darkgray")+
-  geom_point(size=3, alpha=0.8)+
-  facet_wrap(~gender.cat~year.cat~subgenre)+
-  theme_minimal()
-
-### balloon graphics
-
-library(ggpubr)
-ggballoonplot(mdfra, x="year.cat", y="length.cat", facet.by="gender.cat", size="length")
-
-ggplot(mdfra, aes(x=year.cat,y=canon.cat, size=length(gender.cat),color=length))+
-  geom_point()+
-  facet_grid(~gender.cat)
